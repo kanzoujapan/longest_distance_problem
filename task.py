c02b81e706e5d79b7bf4c32262bdb0cfe7c42a43
酒井貫児 <sakaikanji@sakaikanjinoMacBook-Air.local>
2025-07-11 16:38:44 +0900

    all

--- task.py
+++ task.py
@@ -1,6 +1,19 @@
 import sys
 from collections import defaultdict
 
+def element_split(lines):
+    station_info = defaultdict(list)
+    for line in lines:
+        start, end, distance = line.split(',')
+        start = int(start)
+        end = int(end)
+        distance = float(distance)
+
+        station_info[start].append((end, distance))
+        station_info[end].append((start, distance))
+
+    return station_info
+
 
 
 
@@ -13,18 +26,7 @@ if __name__ == '__main__':
 
 
 
-def element_split(lines):
-    station_info = defaultdict(list)
-    for line in lines:
-        start, end, distance = line[0], line[1], line[2]
-        start = int(start)
-        end = int(end)
-        distance = float(distance)
 
-        station_info[start].append((end, distance))
-        station_info[end].append((start, distance))
-
-    return station_info
 
 
 
